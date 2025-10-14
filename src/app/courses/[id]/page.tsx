1981f0656f723cd9c7925fb0205b8c4439438afb
Diaa Elsadek <diaadido1246@gmail.com>
2025-10-14 20:29:47 +0300

    Delete src/app/courses/[id] directory

--- src/app/courses/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CoursePage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Course ID: {params.id}</h1>
-    </div>
-  );
-}
