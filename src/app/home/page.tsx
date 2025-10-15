c8cc39d7cafefa021d57a5b69f3ff388430f3200
Diaa Elsadek <diaadido1246@gmail.com>
2025-10-15 17:42:59 +0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -201,7 +201,7 @@ export default function HomePage() {
                         className="text-2xl font-bold text-indigo-400 drop-shadow-md cursor-pointer"
                         onClick={() => router.push("/home")}
                     >
-                        My College
+                        UniStream22
                     </div>
 
                     <div className="hidden md:flex gap-6">
