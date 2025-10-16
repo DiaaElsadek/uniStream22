5d79089c5cb8f3f07edee3547272247e93a0a992
Diaa Elsadek <diaadido1246@gmail.com>
2025-10-16 04:49:50 +0300

    Update AuthProvider.tsx

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -3,45 +3,29 @@
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
+// const headers = {
+//     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaHZmdXZkeHdod29iZ2xldXlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0OTIwODQsImV4cCI6MjA3NDA2ODA4NH0.P-EefbnljoUmaQ-t03FypD37CRmTDa8Xhv-QMJHndY4",
+//     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaHZmdXZkeHdod29iZ2xldXlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0OTIwODQsImV4cCI6MjA3NDA2ODA4NH0.P-EefbnljoUmaQ-t03FypD37CRmTDa8Xhv-QMJHndY4",
+//     "Content-Type": "application/json",
+// }
+
 const headers = {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaHZmdXZkeHdod29iZ2xldXlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0OTIwODQsImV4cCI6MjA3NDA2ODA4NH0.P-EefbnljoUmaQ-t03FypD37CRmTDa8Xhv-QMJHndY4",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaHZmdXZkeHdod29iZ2xldXlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0OTIwODQsImV4cCI6MjA3NDA2ODA4NH0.P-EefbnljoUmaQ-t03FypD37CRmTDa8Xhv-QMJHndY4",
+    apikey: process.env.NEXT_PUBLIC_API_KEY!, // خليها في .env.local
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN!}`,
     "Content-Type": "application/json",
-}
+};
 
 async function handelLogin(userToken: string): Promise<boolean> {
-    if (!userToken) return false;
-    const query = `?userToken=eq.${userToken}`;
-    const url = `https://udhvfuvdxwhwobgleuyd.supabase.co/rest/v1/AppUser${query}`;
-    try {
-        const res = await fetch(url, {
-            method: "GET",
-            headers: headers,
-        });
-        const data = await res.json();
-        return userToken === data[0]?.userToken;
-    } catch {
-        return false;
-    }
+    const res = await fetch(`/api/auth?userToken=${userToken}`, { headers });
+    const data = await res.json();
+    return data.status;
 }
 
 async function handelRole(userToken: string): Promise<boolean> {
-    if (!userToken) return false;
-    const query = `?userToken=eq.${userToken}`;
-    const url = `https://udhvfuvdxwhwobgleuyd.supabase.co/rest/v1/AppUser${query}`;
-
-    try {
-        const res = await fetch(url, {
-            method: "GET",
-            headers: headers,
-        });
-        const data = await res.json();
-        console.log(data[0]);
-        console.log("Is Admin ? : ", "admin" === data[0]?.Role);
-        return "admin" === data[0]?.Role;
-    } catch {
-        return false;
-    }
+    const res = await fetch(`/api/auth?userToken=${userToken}`, { headers });
+    const data = await res.json();
+    console.log("Data Role", data.user?.Role);
+    return data.user?.Role === "admin";
 }
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
