e7d49ae2e51fa299a5b0b15e94d2330b82ccf926
Diaa Elsadek <diaadido1246@gmail.com>
2025-10-17 04:10:39 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import "./globals.css";
 import AuthProvider from "./AuthProvider";
 
 export const metadata = {
-  title: "HTI Year 4 App",
-  description: "Your Next.js app as a mobile PWA",
+  title: "UniStream22",
+  description:
+    "UniStream22 — منصة لدفعة رابعة بالمعهد التكنولوجي العالي كلية الحاسبات والمعلومات لمتابعة المواد والجداول والأخبار الجامعية بسهولة",
   icons: {
-    icon: "/favicon.png",
+    icon: "../../public/icons/uniStream22.png",
   },
 };
 
@@ -14,7 +15,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#55aaFF" />
+        <meta name="theme-color" content="#55aaff" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              const colors = ['#55aaff', '#8b5cf6', '#22d3ee', '#3b82f6'];
+              let i = 0;
+              setInterval(() => {
+                // تغيّر كل 1 ثانية لتقريب حركة الـ gradient
+                document.querySelector('meta[name="theme-color"]').setAttribute('content', colors[i % colors.length]);
+                i++;
+              }, 1000);
+            `,
+          }}
+        />
       </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
